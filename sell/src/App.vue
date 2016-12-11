3f9b76269250ae93a1ae04fc82fc4831285ff4e4
zhicaizhu123 <zhicaizhu123@126.com>
2016-12-11 22:50:57 +0800

    vue实战项目

---
+++ sell/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">
+        商品
+      </div>
+      <div class="tab-item">
+        评论
+      </div>
+      <div class="tab-item">
+        商家
+      </div>
+    </div>
+    <div class="content">
+      i am content
+    </div>
+  </div>
+</template>
+<script>
+  import header from './components/header/header.vue';
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+<style>
+  #app .tab{
+    display: flex;
+    height: 40px;
+    line-height: 40px;
+  }
+  #app .tab .tab-item{
+    flex: 1;
+    text-align: center;
+  }
+</style>
---
+++ sell/src/components/header/header.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="header">
+		i am header
+	</div>
+</template>
+<script>
+	export default {};
+</script>
+<style>
+	
+</style>
